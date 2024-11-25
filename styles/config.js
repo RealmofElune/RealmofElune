5ab2b1ae82671d3ad12667296b5dd8ebd8a8276e
sisukalat <44116791+sisukalat@users.noreply.github.com>
2024-11-24 23:36:39 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1MhAv9KfLfJu0aoxnJxQ4Qo6GkXaRemkGIxChcdS6Me0";
 
 
 /* ------------------------------------------------------------------- */
