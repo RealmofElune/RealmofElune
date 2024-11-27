572c7faaa21cbf436d3a6c292071fa1b7f0600ea
sisukalat <44116791+sisukalat@users.noreply.github.com>
2024-11-27 17:31:44 -0500

    Update charadex.js

--- styles/charadex.js
+++ styles/charadex.js
@@ -164,6 +164,7 @@ let sheetArrayKeys = (arr) => {
     if (itemArray.indexOf('artistlink')) itemArray[itemArray.indexOf('artistlink')] = { name: 'artistlink', attr: 'href' };
     if (itemArray.indexOf('designerlink')) itemArray[itemArray.indexOf('designerlink')] = { name: 'designerlink', attr: 'href' };
     if (itemArray.indexOf('coownerlink')) itemArray[itemArray.indexOf('coownerlink')] = { name: 'coownerlink', attr: 'href' };
+    if (itemArray.indexOf('coowner2link')) itemArray[itemArray.indexOf('coowner2link')] = { name: 'coowner2link', attr: 'href' };
     if (itemArray.indexOf('image')) itemArray[itemArray.indexOf('image')] = { name: 'image', attr: 'src' };
     return itemArray;
 };
